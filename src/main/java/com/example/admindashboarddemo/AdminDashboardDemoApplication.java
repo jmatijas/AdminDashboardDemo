125e24007ffe19d339e174291a90507bb2129e9e
Josip Matijaš <josip.matijas@gmail.com>
2026-05-14 18:47:23 +0200

    Initial commit - Spring Boot 3.5; Thymeleaf

---
+++ src/main/java/com/example/admindashboarddemo/AdminDashboardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.admindashboarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDashboardDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDashboardDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/admindashboarddemo/AdminDashboardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admindashboarddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
